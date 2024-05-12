ef8ccb9af1d5c7fafed60699a65942ebdd1b5071
integrationninjas <integrationninjas@gmail.com>
2024-05-12 16:29:16 +0530

    deploy to github pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Integration Ninjas
+          Hello, Welcome to Integration Ninjas
         </p>
         <a
           className="App-link"
